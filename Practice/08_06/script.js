210020ea010bbe67a99c30cf818eac6f86178d25
Hramovich Viktoria <nice.gramovich@mail.ru>
2023-03-23 15:05:56 +0300

    lesson 08_06

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,22 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+const object = {
+    name: 'Vika',
+    lastName: 'Gramovich'
+}
+
+const create = function (object) {
+    const article = document.createElement('article');
+    const content = `<h1>Name: ${object.name}</h1>
+    <h2>Last Name: ${object.lastName}<h2/>`;
+    article.innerHTML = content;
+    return article;
+}
+
+const main = function() {
+    const main = document.querySelector('main');
+    main.append(create(object));
+}
+
+main();
